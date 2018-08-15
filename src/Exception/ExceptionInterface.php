cf88438568d1b5ccab50334267a6b1aa7c0edea9
Matthew Weier O'Phinney <matthew@zend.com>
2018-08-15 14:59:32 -0500

    Fixes license docblocks to reference correct repository location

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @see       https://github.com/phly/event-dispatcher for the canonical source repository
+ * @see       https://github.com/phly/phly-event-dispatcher for the canonical source repository
  * @copyright Copyright (c) 2018 Matthew Weier O'Phinney (https:/mwop.net)
- * @license   https://github.com/phly/event-dispatcher/blob/master/LICENSE.md New BSD License
+ * @license   https://github.com/phly/phly-event-dispatcher/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
