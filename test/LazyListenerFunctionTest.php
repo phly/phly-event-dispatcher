f08ed43191a0b2a4a9da2ac71d5a9981251cc4d0
Matthew Weier O'Phinney <matthew@zend.com>
2019-01-14 14:03:35 -0600

    qa: provide tests for lazyListener function
    Adds LazyListenerFunctionTest, to validate that the function does what
    it intends.

---
+++ test/LazyListenerFunctionTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @see       https://github.com/phly/phly-event-dispatcher for the canonical source repository
+ * @copyright Copyright (c) 2018 Matthew Weier O'Phinney (https:/mwop.net)
+ * @license   https://github.com/phly/phly-event-dispatcher/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace PhlyTest\EventDispatcher;
+
+use Phly\EventDispatcher\LazyListener;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+use function Phly\EventDispatcher\lazyListener;
+
+class LazyListenerFunctionTest extends TestCase
+{
+    public function testFunctionReturnsALazyListenerUsingProvidedArguments()
+    {
+        $container = $this->prophesize(ContainerInterface::class)->reveal();
+        $listener = lazyListener($container, TestAsset\Listener::class, 'onTest');
+
+        $this->assertInstanceOf(LazyListener::class, $listener);
+        $this->assertAttributeSame($container, 'container', $listener);
+        $this->assertAttributeSame(TestAsset\Listener::class, 'service', $listener);
+        $this->assertAttributeSame('onTest', 'method', $listener);
+    }
+}
